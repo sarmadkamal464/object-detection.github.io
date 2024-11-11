ede58160e77c0d5b9791008d431892cfe2bca1f2
sarmad <sarmadkamal464@gmail.com>
2024-11-11 17:30:20 +0500

    add multiple videos and refine code.

--- worker.js
+++ worker.js
@@ -1,14 +1,20 @@
 importScripts("https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/ort.min.js");
 
-onmessage = async(event) => {
+onmessage = async (event) => {
     const input = event.data;
-    const output = await run_model(input);
-    postMessage(output);
+    const output = await run_model(input.input);
+    const outObj = {
+        output,
+        index: input.index
+    }
+    postMessage(outObj);
 }
 
 async function run_model(input) {
     const model = await ort.InferenceSession.create("./yolov8n.onnx");
-    input = new ort.Tensor(Float32Array.from(input),[1, 3, 640, 640]);
-    const outputs = await model.run({images:input});
+    input = new ort.Tensor(Float32Array.from(input), [1, 3, 640, 640]);
+    const outputs = await model.run({
+        images: input
+    });
     return outputs["output0"].data;
 }
